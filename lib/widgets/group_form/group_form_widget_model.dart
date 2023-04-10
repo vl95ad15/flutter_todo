b685aabf3e15ecbf7131f33fd34363869734f67f
Vladislav Martsul <martsul.vlad@gmail.com>
2023-04-10 18:18:05 +0300

    add save element

--- lib/widgets/group_form/group_form_widget_model.dart
+++ lib/widgets/group_form/group_form_widget_model.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/domain/entity/group.dart';
+import 'package:hive/hive.dart';
 
 class GroupFormWidgetModel {
   var groupName = '';
-  void saveGroup(BuildContext context) {
-    print(groupName);
+  void saveGroup(BuildContext context) async {
+    if (groupName.isEmpty) return;
+    if (!Hive.isAdapterRegistered(1)) {
+      Hive.registerAdapter(GroupAdapter());
+    }
+    final box = await Hive.openBox<Group>('groups_box');
+    final group = Group(name: groupName);
+    await box.add(group);
+    Navigator.of(context).pop();
   }
 }
 
