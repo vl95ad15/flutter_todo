1f0010387206e4ed6e168f4ed97f375bca8a354b
Vladislav Martsul <martsul.vlad@gmail.com>
2023-04-10 17:59:10 +0300

    add hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/widgets/app/my_app.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   runApp(const MyApp());
 }
