00718d2b5ba6d622769d3e771bfc71227d5cf9cc
Vladislav Martsul <martsul.vlad@gmail.com>
2023-04-11 17:11:11 +0300

    add save/read/delete saved group tasks in model

--- lib/widgets/tasks/tasks_widget_model.dart
+++ lib/widgets/tasks/tasks_widget_model.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/domain/entity/group.dart';
-import 'package:hive/hive.dart';
+import 'package:flutter_todo/domain/entity/task.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class TasksWidgetModel extends ChangeNotifier {
   int groupKey;
   late final Future<Box<Group>> _groupBox;
+
+  var _tasks = <Task>[];
+  List<Task> get tasks => _tasks.toList();
+
   Group? _group;
   Group? get group => _group;
 
@@ -22,12 +27,28 @@ class TasksWidgetModel extends ChangeNotifier {
     notifyListeners();
   }
 
+  void _readTasks() {
+    _tasks = _group?.tasks ?? <Task>[];
+    notifyListeners();
+  }
+
+  void _setupListenTasks() async {
+    final box = await _groupBox;
+    _readTasks();
+    box.listenable(keys: <dynamic>[groupKey]).addListener(_readTasks);
+  }
+
+  void deleteTask(int groupIndex) {
+    _group?.tasks?.deleteFromHive(groupIndex);
+  }
+
   void _setup() {
     if (!Hive.isAdapterRegistered(1)) {
       Hive.registerAdapter(GroupAdapter());
     }
     _groupBox = Hive.openBox<Group>('groups_box');
     _loadGroup();
+    _setupListenTasks();
   }
 }
 
