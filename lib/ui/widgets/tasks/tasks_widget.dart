e73a257d45fcc5e52c4779ae953a60b76386b0eb
Vladislav Martsul <martsul.vlad@gmail.com>
2023-04-21 13:57:39 +0300

    didChangeDependencies > initState refactoring

--- lib/ui/widgets/tasks/tasks_widget.dart
+++ lib/ui/widgets/tasks/tasks_widget.dart
@@ -3,29 +3,29 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutter_todo/ui/widgets/tasks/tasks_widget_model.dart';
 
 class TasksWidget extends StatefulWidget {
-  const TasksWidget({super.key});
+  final int groupKey;
+  const TasksWidget({
+    super.key,
+    required this.groupKey,
+  });
 
   @override
   State<TasksWidget> createState() => _TasksWidgetState();
 }
 
 class _TasksWidgetState extends State<TasksWidget> {
-  TasksWidgetModel? _model;
+  late final TasksWidgetModel _model;
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-
-    if (_model == null) {
-      final groupKey = ModalRoute.of(context)!.settings.arguments as int;
-      _model = TasksWidgetModel(groupKey: groupKey);
-    }
+  void initState() {
+    super.initState();
+    _model = TasksWidgetModel(groupKey: widget.groupKey);
   }
 
   @override
   Widget build(BuildContext context) {
     return TasksWidgetModelProvider(
-      model: _model!,
+      model: _model,
       child: const TasksWidgetBody(),
     );
   }
